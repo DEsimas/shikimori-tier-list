62d05977ae22878f3b15c296916258777b36624f
DEsimas <simasuh@gmail.com>
2022-08-19 15:34:57 +0300

    add Root component

---
+++ src/Pages/Root.js
@@ -0,0 +1,6 @@
+export default function Root() {
+
+  return (
+    <h1>uwu</h1>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import Root from './Pages/Root';
 
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
