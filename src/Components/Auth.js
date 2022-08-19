3030405096b121bbf8cc6980c10d46fef490cdc9
DEsimas <simasuh@gmail.com>
2022-08-19 19:03:33 +0300

    form functions

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { Auth as AuthRequest } from './../Requests/Auth.js';
 
 export default function Auth() {
+  const [ username, setUsername ] = useState("");
+  const [ password, setPassword ] = useState("");
+  
+  const navigate = useNavigate();
 
-    return (
-        <div className='auth'>
-            <h3 className='auth-header'>Sign In</h3>
-            <input className='auth-input' type={'text'} placeholder='Username' />
-            <input className='auth-input' type={'password'} placeholder='Password' />
-            <button className='auth-button'>Continue</button>
-            <a className='auth-link'>Create an account</a>
-        </div>
-    );
+  function submit() {
+    AuthRequest({ username, password });
+    navigate('/uwu');
+  }
+
+  return (
+    <div className='auth'>
+      <h3 className='auth-header'>Sign In</h3>
+      <input className='auth-input' type={'text'} placeholder='Username' onChange={e => setUsername(e.target.value)} />
+      <input className='auth-input' type={'password'} placeholder='Password' onChange={e => setPassword(e.target.value)} />
+      <button onClick={submit} className='auth-button'>Continue</button>
+      <Link to='/Registration' className='auth-link'>Create an account</Link>
+    </div>
+  );
 }
---
+++ src/Requests/Auth.js
@@ -0,0 +1,3 @@
+export function Auth(body) {
+    console.log(body);
+}
