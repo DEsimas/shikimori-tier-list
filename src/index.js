77b3e03a03152bba097011828ed323a7fd05863d
DEsimas <simasuh@gmail.com>
2022-08-19 15:48:33 +0300

    add routing

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom/client';
 import Root from './Pages/Root';
 
 import './index.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Root />} />
+    </Routes>
+  </BrowserRouter>
 );
