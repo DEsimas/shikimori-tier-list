b16493b848ec6c889cc4bb5ab4cd02d057569abb
DEsimas <simasuh@gmail.com>
2022-08-19 23:52:27 +0300

    Registration html

--- src/Pages/Registration.js
+++ src/Pages/Registration.js
@@ -4,7 +4,12 @@ export default function Registration() {
 
   return (
     <div className='registration'>
-      
+      <h3 className='registration-header'>Registration</h3>
+      <input className='registration-input' placeholder='username' />
+      <input className='registration-input' placeholder='password'/>
+      <input className='registration-input' placeholder='repeat password' />
+      <button className='registration-button'>Continue</button>
+      <span className='registration-error'></span>
     </div>
   );
 }
