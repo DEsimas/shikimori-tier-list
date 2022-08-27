2669130fcb41682a560b77cca391beaeac620341
DEsimas <simasuh@gmail.com>
2022-08-27 15:40:24 +0300

    link second step

--- src/Components/Link.js
+++ src/Components/Link.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-import { getCode as getCodeRequest } from '../Requests/Link'
+import { getCode as getCodeRequest, verifyCode } from '../Requests/Link'
 
 export default function Link() {
     const [lang, setLang] = useState('en')
     const [code, setCode] = useState()
+    const [username, setUsername] = useState('')
     const [codeError, setCodeError] = useState()
+    const [nameError, setNameError] = useState()
+
+    const navigate = useNavigate()
 
     async function getCode(e) {
         e.target.setAttribute('disabled', 'disabled')
-        const { data, error } = await getCodeRequest()
+        const { data, error } = await getCodeRequest(username)
+        e.target.removeAttribute('disabled')
+        if(error) setNameError(error)
+        else setCode(data.code)
+    }
+
+    async function finish(e) {
+        e.target.setAttribute('disabled', 'disabled')
+        const { data, error } = await verifyCode()
         e.target.removeAttribute('disabled')
         if(error) setCodeError(error)
-        else setCode(data.code)
+        else navigate('/')
     }
 
     return (
@@ -21,17 +34,18 @@ export default function Link() {
             <h2 className='link-s1-header'>{lang === 'en' ? 'It\'s time to link your account with shikimori' : 'Пора привязать ваш шикимори аккаунт'}</h2>
             <div className='link-s1'>
                 <p className='link-s1-para'>{lang === 'en' ? '1)Enter your shikimori nickname' : '1)Введите ваш ник на шикимори'}</p>
-                <input className='input-s1-input' type='text' placeholder={lang === 'en' ? 'nickname' : 'ник'} />
-                <button>{lang === 'en' ? 'Next' : 'Дальше'}</button>
+                <input onChange={e => setUsername(e.target.value)} className='input-s1-input' type='text' placeholder={lang === 'en' ? 'nickname' : 'ник'} />
+                <button className='link-s1-button' onClick={getCode}> {lang === 'en' ? 'Get code' : 'Получить код'} </button>
+                <span className='link-s1-error'>{nameError}</span>
             </div>
-            <div className='link-s2'>
+            <div className={`link-s2 ${code ? '' : 'hide'}`}>
                 <p className='link-s2-para'>
                     {lang === 'en' ?
                         '2)Open settings on shikimori and state this code as "website" (after linking you can remove it)' :
                         '2)Перейдите в настройки вашего шикимори аккаунта и в поле "сайт" укажите код (после привязки можно убрать его)'}
                 </p>
-                <button className='link-s2-button' onClick={getCode}> {lang === 'en' ? 'Get code' : 'Получить код'} </button>
-                <span className='link-s2-code'>{code}</span>
+                <span className='link-s2-code'>Code: {code}</span>
+                <button onClick={finish} className='link-s2-button'>{lang === 'en' ? 'Done' : 'Готово'}</button>
                 <span className='link-s2-error'>{codeError}</span>
             </div>
         </div>
--- src/Requests/Link.js
+++ src/Requests/Link.js
@@ -1,5 +1,17 @@
 export async function getCode(username) {
     console.log({username})
+    // return {
+    //     data: undefined,
+    //     error: 'Server is undefined'
+    // }
+    return {
+        data: { code: '432oij5r3i43kl' },
+        error: undefined
+    }
+}
+
+export async function verifyCode() {
+    console.log('verifying code...')
     return {
         data: undefined,
         error: 'Server is undefined'
