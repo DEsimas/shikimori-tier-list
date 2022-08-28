f80c324a3c13c78a3c71a0e7598f88f41d8b93ac
DEsimas <simasuh@gmail.com>
2022-08-28 13:39:53 +0300

    register request

--- src/Requests/Auth.js
+++ src/Requests/Auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export async function Auth(body) {
     console.log(body)
     return {
@@ -7,9 +9,40 @@ export async function Auth(body) {
 }
 
 export async function Registration(body) {
-    console.log(body)
-    return {
+    if (body.username === undefined ||
+        body.username === '' ||
+        body.username?.length < 3) return {
+            data: undefined,
+            error: 'Username is too short'
+        }
+
+    if (body.username?.length >= 20) return {
         data: undefined,
-        error: 'Server is not defined'
+        error: 'Username is too long'
+    }
+
+    if (body.password === undefined ||
+        body.password === '' ||
+        body.password?.length <= 5) return {
+            data: undefined,
+            error: 'Password is too short'
+        }
+
+    if (body.password?.length >= 20) return {
+        data: undefined,
+        error: 'Password is too long'
+    }
+
+    try {
+        const res = await axios.post(process.env.REACT_APP_SERVER_URL + '/register', { ...body })
+        return {
+            data: res.data,
+            error: undefined
+        }
+    } catch (err) {
+        return {
+            data: undefined,
+            error: err.response.data.error
+        }
     }
 }
